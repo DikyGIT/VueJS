12c87f38c1d862c7d8685776bdcd3aa2fc4107b1
DikyGit <dikyoga27@gmail.com>
2021-11-24 09:14:15 +0700

    Menambahkan file new vuegogo

---
+++ vuegogo/src/views/Profile.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="profile">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-6">
+          <div class="d-flex align-items-center">
+            <div class="flex-shrink-0">
+              <img
+                src="../assets/me.png"
+                alt="Profile Logo"
+                class="profile-logo"
+              />
+            </div>
+            <div class="flex-grow-1 ms-3 text-start ps-3">
+              <h3>{{ firstName }} {{ lastName }}</h3>
+              <p>{{ penjelasan }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+
+export default {
+  name: "profile",
+  data: function () {
+    return {
+      firstName: "Diky",
+      lastName: "Prayoga",
+      penjelasan:
+        "This is some content from a media component. You can replace this with any content and adjust it as needed",
+    };
+  },
+  components: {},
+};
+</script>
+
+<style scoped>
+.profile-logo {
+  width: 100px;
+  height: 100px;
+  border-radius: 100%;
+}
+</style>
