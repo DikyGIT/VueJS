5f42e77347acc7f713fba7f400f3ff6ef6735105
DikyGit <dikyoga27@gmail.com>
2021-11-29 11:37:18 +0700

    Menambahkan Project Bootstrap Vue

---
+++ test-bootstrap-vue/src/components/BootstrapVue.vue
@@ -0,0 +1,285 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <div class="container">
+        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="#">Link</b-nav-item>
+            <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <b-form-input
+                size="sm"
+                class="mr-sm-2"
+                placeholder="Search"
+              ></b-form-input>
+              <b-button size="sm" class="my-2 my-sm-0" type="submit"
+                >Search</b-button
+              >
+            </b-nav-form>
+          </b-navbar-nav>
+        </b-collapse>
+      </div>
+    </b-navbar>
+
+    <b-jumbotron class="jumbotron">
+      <template #header>BootstrapVue</template>
+
+      <template #lead>
+        This is a simple hero unit, a simple jumbotron-style component for
+        calling extra attention to featured content or information.
+      </template>
+
+      <hr class="my-4" />
+
+      <p>
+        It uses utility classes for typography and spacing to space content out
+        within the larger container.
+      </p>
+
+      <b-button variant="primary" href="#">Do Something</b-button>
+      <b-button variant="success" href="#">Do Something Else</b-button>
+    </b-jumbotron>
+
+    <section class="card bg-info text-dark">
+      <b-container class="mb-3">
+        <b-row>
+          <b-col>
+            <div>
+              <b-card
+                title="Card Title"
+                img-src="https://picsum.photos/600/300/?image=25"
+                img-alt="Image"
+                img-top
+                tag="article"
+                style="max-width: 20rem"
+                class="mb-2"
+              >
+                <b-card-text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </b-card-text>
+
+                <b-button href="#" variant="primary">Go somewhere</b-button>
+              </b-card>
+            </div>
+          </b-col>
+          <b-col>
+            <div>
+              <b-card
+                title="Card Title"
+                img-src="https://picsum.photos/600/300/?image=25"
+                img-alt="Image"
+                img-top
+                tag="article"
+                style="max-width: 20rem"
+                class="mb-2"
+              >
+                <b-card-text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </b-card-text>
+
+                <b-button href="#" variant="primary">Go somewhere</b-button>
+              </b-card>
+            </div>
+          </b-col>
+          <b-col>
+            <div>
+              <b-card
+                title="Card Title"
+                img-src="https://picsum.photos/600/300/?image=25"
+                img-alt="Image"
+                img-top
+                tag="article"
+                style="max-width: 20rem"
+                class="mb-2"
+              >
+                <b-card-text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </b-card-text>
+
+                <b-button href="#" variant="primary">Go somewhere</b-button>
+              </b-card>
+            </div>
+          </b-col>
+        </b-row>
+      </b-container>
+    </section>
+
+    <section class="table">
+      <b-container>
+        <b-row>
+          <b-col cols="7" class="mr-4">
+            <b-table striped hover :items="items" :fields="fields"></b-table>
+          </b-col>
+          <b-col cols="4" class="text-justify pt-4">
+            <h2>{{ nameTable }}</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+              reiciendis omnis porro facilis ea odio non voluptatum nobis
+              eligendi in! Explicabo rerum facere perferendis illum quam id
+              placeat, possimus libero, optio itaque rem veniam. Cum numquam
+              sequi dicta eveniet qui! Lorem ipsum dolor sit amet consectetur
+              adipisicing elit.
+            </p>
+          </b-col>
+        </b-row>
+      </b-container>
+    </section>
+
+    <section class="form bg-info text-light">
+      <b-container>
+        <b-row class="text-left">
+          <b-col cols="6">
+            <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+              <b-form-group
+                id="input-group-1"
+                label="Email address:"
+                label-for="input-1"
+              >
+                <b-form-input
+                  id="input-1"
+                  v-model="form.email"
+                  type="email"
+                  placeholder="Enter email"
+                  required
+                ></b-form-input>
+              </b-form-group>
+
+              <b-form-group
+                id="input-group-2"
+                label="Your Name:"
+                label-for="input-2"
+              >
+                <b-form-input
+                  id="input-2"
+                  v-model="form.name"
+                  placeholder="Enter name"
+                  required
+                ></b-form-input>
+              </b-form-group>
+
+              <b-form-group
+                id="input-group-3"
+                label="Food:"
+                label-for="input-3"
+              >
+                <b-form-select
+                  id="input-3"
+                  v-model="form.food"
+                  :options="foods"
+                  required
+                ></b-form-select>
+              </b-form-group>
+
+              <b-button type="submit" variant="primary" class="mr-2"
+                >Submit</b-button
+              >
+              <b-button type="reset" variant="danger">Reset</b-button>
+            </b-form>
+          </b-col>
+          <b-col cols="6" class="pt-3">
+            <h1>This is Form</h1>
+            <p class="text-justify">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed
+              illum nam culpa dicta a necessitatibus incidunt doloremque
+              sapiente eligendi veritatis, architecto aliquid commodi optio,
+              pariatur ea ut accusantium soluta facilis nesciunt. Ut temporibus,
+              facere laborum, id sed maiores fugit cum excepturi esse, odit
+              exercitationem illum eius odio voluptates. In velit excepturi
+              veniam pariatur voluptatum distinctio aspernatur consequatur
+              voluptate, recusandae assumenda?
+            </p>
+          </b-col>
+        </b-row>
+      </b-container>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BootstrapVue",
+
+  data() {
+    return {
+      // Note `isActive` is left out and will not appear in the rendered table
+      nameTable: "This The Table",
+      fields: ["first_name", "last_name", "age"],
+      items: [
+        {
+          isActive: true,
+          age: 40,
+          first_name: "Dickerson",
+          last_name: "Macdonald",
+        },
+        { isActive: false, age: 21, first_name: "Larsen", last_name: "Shaw" },
+        { isActive: false, age: 89, first_name: "Geneva", last_name: "Wilson" },
+        { isActive: true, age: 38, first_name: "Jami", last_name: "Carney" },
+        { isActive: true, age: 38, first_name: "Jami", last_name: "Carney" },
+      ],
+      form: {
+        email: "",
+        name: "",
+        food: null,
+        checked: [],
+      },
+      foods: [
+        { text: "Select One", value: null },
+        "Carrots",
+        "Beans",
+        "Tomatoes",
+        "Corn",
+      ],
+      show: true,
+    };
+  },
+
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(event) {
+      event.preventDefault();
+      // Reset our form values
+      this.form.email = "";
+      this.form.name = "";
+      this.form.food = null;
+      this.form.checked = [];
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.jumbotron {
+  margin-bottom: 0;
+}
+
+.card {
+  padding: 30px 0 10px 0;
+}
+
+.table {
+  padding: 30px 0 30px 0;
+}
+
+.form {
+  padding: 30px 0 30px 0;
+}
+</style>
